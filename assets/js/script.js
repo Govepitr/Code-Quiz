3eb0b0d9a10ba57c85e3ac480f5f31285b599e51
Dan Arbelo <govepitr@gmail.com>
2021-01-04 18:54:53 -0700

    deep restructure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,123 @@
+var hiScore = document.getElementById("hi-hiScore");
+var questions = document.getElementById("questions").innerHTML = "/assets/js/questions.js";
+var timer = document.getElementById("count-down-timer");
+var startQuiz = document.getElementById("start");
+var timerId;
+var time = 75;
+var questionArray = 0;
+
+
 var questions = [{
 
-    quizQuestion: "Arrays in JavaScript can be used to store _____.",
-    answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-    correct:"4. all of the above"
-  },
-  {
-    quizQuestion: "String Values must be enclosed within ____ when being assigned to variables.",
-    answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
-    correct:"3. quotes"
-  },
-  {
-    quizQuestion: "The condition in an if/else statement is enclosed with ____.",
-    answers: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
-    correct:"2. curly brackets"
-  },
-  {
-    quizQuestion: "Commonly used data types DO NOT include:",
-    answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-    correct:"3. alerts"
-  },
-  {
-    quizQuestion: "A very useful tool for users during development and debugging for printing content to the debugger is:",
-    answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console log"],
-    correct:"4. console log"
-  }
+  quizQuestion: "Arrays in JavaScript can be used to store _____.",
+  answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+  correct:"4. all of the above"
+},
+{
+  quizQuestion: "String Values must be enclosed within ____ when being assigned to variables.",
+  answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+  correct:"3. quotes"
+},
+{
+  quizQuestion: "The condition in an if/else statement is enclosed with ____.",
+  answers: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+  correct:"2. curly brackets"
+},
+{
+  quizQuestion: "Commonly used data types DO NOT include:",
+  answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+  correct:"3. alerts"
+},
+{
+  quizQuestion: "A very useful tool for users during development and debugging for printing content to the debugger is:",
+  answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console log"],
+  correct:"4. console log"
+}
 ]
 
 
-var score = 0;
-var currentQuestion = -1;
-var timer;
-var time = 0;
-var generateBtn = document.querySelector("#start");
 
 
-//Countdown Timer
 
-function start() {
-
-  time = ":",75;
-  document.getElementById("time").innerHTML = time;
-
-  timer = setInterval(1000); {
-    time--;
-    document.getElementById("time").innerHTML = time;
-  //The endGame function will begin at any point that the timer has reached 0
-    if(time <= 0) {
-        clearInterval(timer);
-        endGame();
-    }
-  } 
-
-  next();
-
+//Page loads up timer values defined above
+function loadQuiz() {
+  timerId = setInterval("count-down-timer", 1000);
+  timer.textContent = time;
+  startQuestions();
 }
 
-// Timer has been stopped, so the game has ended
-function endGame() {
-  clearInterval(timer);
+// This function starts to pull the questions 1 at a time
+function startQuestions(questions) {
+  var currentQuestion = questions[questionArray];
+    const questionTextArea = document.getElementById("quiz-title");
+    questionTextArea.textContent = currentQuestion.quizQuestions;
+    const answersEl = document.getElemenId("questions");
+    answersEl.innerHTML = "";
+    currentQuestion.answers.forEach(function (answer) {
+  var answerNode = document.createElement("button");
+      answerNode.setAttribute("class", "answer button");
+      answerNode.setAttribute("value", answer);
+      answerNode.textContent = answer;
+      answerNode.onclick = questionClick;
+      answersEl.appendChild(answerNode);
+  });
+}
+
+function questionClick(event) {
+  var correct = event.target.textContent;
+  var correct = questions(currrentQuestionIndex).correct;
+  if (correct === answer) {
+    var correct = "Most excellent, that's correct!";
+    document.getElementById("quiz-content").innerHTML = window.correct
+    document.getElementById("key").style.borderTop = "1px solid #a19f9fca";
+  } else {
+    time -= 10;
+    result.textContent = "Oof, sorry. I need to take some time away now. That was incorrect!";
+    document.getElementById("key").style.borderTop = "1px solid #a19f9fca";
+  }
+  questionArray++;
+  if (time <= 0) {
+    endQuiz();
+  } else {
+    startQuestions();
+
+  }
+}
+
+function countDown() {
+  time== "";
+  timer.textContent = time;
+  if (time <= 0) {
+    endQuiz();
+  }
+  console.log(countDown);
+}
+
+function endQuiz() {
+  clearInterval(timerId);
+
+  // replace the id="quiz-title" content w/a times up message & button to view score
 
   var questions = `
   <h2>Game Over, sorry!</h2>
-  <h3>You scored a ` + score + ` /100!</h3>
-  <h3>That means you totally nailed ` + score/20 + ` questions like a digital boss!</h3>
+  <h3>You scored a ` + hiScore + ` /100!</h3>
+  <h3>That means you totally nailed ` + hiScore/20 + ` questions like a digital boss!</h3>
   <input type="text" maxlength="3" id="initials" placeholder="Please enter your initials">
   <button onclick="savescore()">Save Score!</button>`;
   
   document.getElementById("start-quiz").innerHTML = questions;
 }
 
-//Locallly store the player's high score
+//Locallly store the player's high hiScore
 function savescore() {
-  localStorage.setItem("highscore", score);
+  localStorage.setItem("highscore", hiScore);
   localStorage.setItem("highscoreinitials", document.getElementById('initials').value);
   getScore();
 }
 
 function getScore() {
   var questions = `
-  <h2>` + localStorage.getItem("highscoreinitials") + ` is truly "The One" with a score of:</h2>
+  <h2>` + localStorage.getItem("highscoreinitials") + ` is truly "The One" with a hiScore of:</h2>
   <h1>` + localStorage.getItem("highscore") + `<h1><br>
   
   <button onclick="clearScore()">Clear Score</button><button onclick="resetGame()">Play Again?</button>`;
@@ -86,7 +125,7 @@ function getScore() {
   document.getElementById("start-quiz").innerHTML = questions;
 }
 
-// Wipe the score board clean removing the names, scores, everything as long as they click 'clear score'
+// Wipe the hiScore board clean removing the names, scores, everything as long as they click 'clear hiScore'
 function clearScore() {
   localStorage.setItem("highscore", "");
   localStorage.setItem("highscoreinitials", "");
@@ -95,70 +134,9 @@ function clearScore() {
 }
 
 //reset the game
-function resetGame() {
-  clearInterval(timer);
-  score = 0;
-  currentQuestion = -1;
-  time = 0;
-  timer = null;
-
-  document.getElementById("time").innerHTML = time;
-
-  var questions = `
-  <h1>
-    Coding Quiz Challenge
-  </h1>
-  <button>
-    Start Quiz
-  </button>
-  <button onclick="start()">Start Quiz</button>`;
-
-  document.getElementById("start-quiz").innerHTML = questions;
-
-}
-
-//Lose 10 seconds from their time if they select the incorrect answer
-function incorrect() {
-  time -= 10;
-  document.getElementById("key").style.borderTop = "1px solid #a19f9fca";
-  next();
-}
 
 
 
 
-// function next() {
-//   currentQuestion++;
-
-//   if (currentQuestion > questions.length - 1) {
-//       endGame();
-//       return;
-//   }
-
-  var questions = "<quiz-questions>" + questions[currentQuestion].title + "</quiz-questions>"
-
-  for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].answers.length; buttonLoop++) {
-      var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
-      buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].answers[buttonLoop]);
-      if (questions[currentQuestion].answers[buttonLoop] == questions[currentQuestion].answer) {
-          buttonCode = buttonCode.replace("[ANS]", "correct()");
-      } else {
-          buttonCode = buttonCode.replace("[ANS]","I'm Sorry, that's incorrect!()");
-      }
-      questions += buttonCode
-  }
-
-
-  // document.getElementById("quizBody").innerHTML = questions;
-
-
-
-//function to validate the correct answer on click
-
-//end quiz function(if time runs outerHeight, hide the questions, show the end screenfollowed by the high score)
-
-// save high score
-
-///quiz end function (restart options display highscore)
-
-generateBtn.addEventListener("click", start);
+// generateBtn.addEventListener("click", start);;
+loadQuiz();
