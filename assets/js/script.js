4c48cab680588826154acaaceccbf059c7ec6079
Dan Arbelo <govepitr@gmail.com>
2021-01-08 08:47:48 -0700

    Restructured JS/HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,142 +1,170 @@
-var hiScore = document.getElementById("hi-hiScore");
-var questions = document.getElementById("questions").innerHTML = "/assets/js/questions.js";
-var timer = document.getElementById("count-down-timer");
-var startQuiz = document.getElementById("start");
+// variables that keep track of the state for the quiz
+var currrentQuestionIndex = 0;
+var time = questions.length * 15;
 var timerId;
-var time = 75;
-var questionArray = 0;
 
+// DOM reference elements
 
-var questions = [{
+var timeEl = document.querySelector("#count-down-timer");
+var startBtn = document.querySelector("#start-quiz-btn");
+var submitBtn = document.querySelector("#initials-submit-button");
+var titleScreen = document.querySelector("#quiz-question");
+var quizScreen = document.querySelector("#answer");
+var highScoreScreen = document.querySelector("#highscore");
+var highScoreDisplay = document.querySelector("#highscore-section");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#correct-wrong");
 
-  quizQuestion: "Arrays in JavaScript can be used to store _____.",
-  answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-  correct:"4. all of the above"
-},
-{
-  quizQuestion: "String Values must be enclosed within ____ when being assigned to variables.",
-  answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
-  correct:"3. quotes"
-},
-{
-  quizQuestion: "The condition in an if/else statement is enclosed with ____.",
-  answers: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
-  correct:"2. curly brackets"
-},
-{
-  quizQuestion: "Commonly used data types DO NOT include:",
-  answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-  correct:"3. alerts"
-},
-{
-  quizQuestion: "A very useful tool for users during development and debugging for printing content to the debugger is:",
-  answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console log"],
-  correct:"4. console log"
-}
-]
+var questionsEl = document.querySelector("#questions");
+var choicesEl = document.querySelector("#answers");
 
+//create functions to start the game
+function startQuiz() {
+  //hide the start screen after beginning quiz
+  titleScreen.setAttribute("class", "hide");
 
+  //un-hide questions section
+  quizScreen.setAttribute("class", "show");
 
+  //timer begins
+  timerId = setInterval(tick, 1000);
 
+  //show beginning of timer
+  timeEl.textContent = time;
 
-//Page loads up timer values defined above
-function loadQuiz() {
-  timerId = setInterval("count-down-timer", 1000);
-  timer.textContent = time;
-  startQuestions();
+  getQuestion();
 }
 
-// This function starts to pull the questions 1 at a time
-function startQuestions(questions) {
-  var currentQuestion = questions[questionArray];
-    const questionTextArea = document.getElementById("quiz-title");
-    questionTextArea.textContent = currentQuestion.quizQuestions;
-    const answersEl = document.getElemenId("questions");
-    answersEl.innerHTML = "";
-    currentQuestion.answers.forEach(function (answer) {
-  var answerNode = document.createElement("button");
-      answerNode.setAttribute("class", "answer button");
-      answerNode.setAttribute("value", answer);
-      answerNode.textContent = answer;
-      answerNode.onclick = questionClick;
-      answersEl.appendChild(answerNode);
-  });
-}
+// create a second taken off of the clock
+function tick() {
+  //timne being updated
+  time--;
+  timeEl.textContent = time;
 
-function questionClick(event) {
-  var correct = event.target.textContent;
-  var correct = questions(currrentQuestionIndex).correct;
-  if (correct === answer) {
-    var correct = "Most excellent, that's correct!";
-    document.getElementById("quiz-content").innerHTML = window.correct
-    document.getElementById("key").style.borderTop = "1px solid #a19f9fca";
-  } else {
-    time -= 10;
-    result.textContent = "Oof, sorry. I need to take some time away now. That was incorrect!";
-    document.getElementById("key").style.borderTop = "1px solid #a19f9fca";
-  }
-  questionArray++;
+  //veriify if time has run out
   if (time <= 0) {
-    endQuiz();
-  } else {
-    startQuestions();
-
+    quizEnd();
   }
 }
 
-function countDown() {
-  time== "";
-  timer.textContent = time;
-  if (time <= 0) {
-    endQuiz();
+  function getQuestion() {
+    // pull current question from array
+    var currentQuestion = questions[currrentQuestionIndex];
+
+    //update the title w/the current question
+    var titleEl = document.getElementById("questions")
+    titleEl.textContent = currentQuestion.title;
+
+    //remove old question and choices
+    choicesEl.innerHTML = "";
+
+    //loop choices
+    currentQuestion.choices.forEach(function(choice, I) {
+      //create buttons for each choices
+      var choiceNode = document.createElement("button");
+      choiceNode.setAttribute("class", "choice");
+      choiceNode.setAttribute("value", choice);
+
+      choiceNode.textContent = i + 1 + ". " + choice;
+
+      //add click even listener to every choice
+      choiceNode.onClick = questionClick;
+
+      // load questions to page
+      choices.appendChild(choiceNode);
+    });
   }
-  console.log(countDown);
+
+// click on question to generate a new question, end the quiz if on last question, but also deduct time for incorrect selections
+  function questionClick() {
+    //verify if selection is incorrect
+    if (this.value !== questions[currrentQuestionIndex].answer) {
+      //remove time
+      time -= 15;
+
+      if (time < 0) {
+        time = 0;
+      }
+
+      //update time text on page
+      timeEl.textContent = time;
+
+      feedbackEl.textContent = "Oof, sorry. I need to take some time away now. That was incorrect!";
+    } else {
+
+      feedbackEl.textContent = "Most excellent, that's correct!";
+    }
+
+    // keep correct/incorrect feedback on page for a second
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    // next question will appear via index
+    currrentQuestionIndex++;
+
+    //verify hopw many questions remain
+    if (currrentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+  }
+
+//end the quiz via function
+  function quizEnd() {
+    //timer stops
+    clearInterval(timerId);
+
+    //show end of quiz screen
+    var highscoreFinalEl = document.querySelector("#highscore")
+    ;
+    highscoreFinalEl.setAttribute("class", "show");
+
+    // reveal final score
+    var finalScoreEl = document.querySelector("#final-score");
+    finalScoreEl.textContent = time;
+
+    //hide the questions
+    quizScreen.setAttribute("class", "hide");
+  }
+
+//saving the high score
+function saveHighscore() {
+  //get the value of the input
+  var saved = initialsEl.value.trim();
+
+  //verify they entered something and didn't leave it blank
+  if (saved !== "") {
+    //get the scored saved to localStorage, but if there isn't any set it to the empty array
+    var highscores = parseFloat(window.localStorage.getItem("highscores")) || [];
+
+    //format the retrieved item for the user
+    var newScore = {
+      score: time, initials: initials
+    };
+
+    //save info to localstorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", parseFloat.stringify(highscores));
+
+    //redirect to highscore page
+    window.location.href = "highScore.html";
+  }
 }
 
-function endQuiz() {
-  clearInterval(timerId);
-
-  // replace the id="quiz-title" content w/a times up message & button to view score
-
-  var questions = `
-  <h2>Game Over, sorry!</h2>
-  <h3>You scored a ` + hiScore + ` /100!</h3>
-  <h3>That means you totally nailed ` + hiScore/20 + ` questions like a digital boss!</h3>
-  <input type="text" maxlength="3" id="initials" placeholder="Please enter your initials">
-  <button onclick="savescore()">Save Score!</button>`;
-  
-  document.getElementById("start-quiz").innerHTML = questions;
+function checkForEnter(event) {
+  // "13" is representing the enter key
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
 }
 
-//Locallly store the player's high hiScore
-function savescore() {
-  localStorage.setItem("highscore", hiScore);
-  localStorage.setItem("highscoreinitials", document.getElementById('initials').value);
-  getScore();
-}
+//the user clicks the button for initials submission
+submitBtn.onclick() = saveHighscore;
 
-function getScore() {
-  var questions = `
-  <h2>` + localStorage.getItem("highscoreinitials") + ` is truly "The One" with a hiScore of:</h2>
-  <h1>` + localStorage.getItem("highscore") + `<h1><br>
-  
-  <button onclick="clearScore()">Clear Score</button><button onclick="resetGame()">Play Again?</button>`;
+//or they click to start the quiz
+startBtn.onClick() = startQuiz;
 
-  document.getElementById("start-quiz").innerHTML = questions;
-}
-
-// Wipe the hiScore board clean removing the names, scores, everything as long as they click 'clear hiScore'
-function clearScore() {
-  localStorage.setItem("highscore", "");
-  localStorage.setItem("highscoreinitials", "");
-
-  resetGame();
-}
-
-//reset the game
-
-
-
-
-// generateBtn.addEventListener("click", start);;
-loadQuiz();
+initialsEl.onkeyup() = checkForEnter;
